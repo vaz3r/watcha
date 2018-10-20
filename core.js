c90f5dc95c23c55d2795d2ac8c54c68c0d18f475
Vazer Rahmani <vaz3r@outlook.com>
2018-10-20 19:10:23 +0100

    v1.1

--- core.js
+++ core.js
@@ -10,17 +10,10 @@ function loadTodaysGames() {
         }
     };
 
-    var utc = new Date().toJSON().slice(0,10).replace(/-/g,'/');
-    url = "https://stats.nba.com/stats/scoreboardV2?DayOffset=0&LeagueID=00&gameDate=" + utc;
+    var utc = new Date().toJSON().slice(0,10).replace(/-/g,'');
+    url = "http://watchat-watchatcore.a3c1.starter-us-west-1.openshiftapps.com/scoreboard/" + utc;
 
     httpClient.open("GET", url, true);
-    httpClient.setRequestHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36');
-    httpClient.setRequestHeader('Origin', 'stats.nba.com');
-    httpClient.setRequestHeader('Host', 'stats.nba.com');
-    httpClient.setRequestHeader('Referer', 'https://stats.nba.com/scores/10/19/2018');
-    httpClient.setRequestHeader('x-nba-stats-origin', 'stats');
-    httpClient.setRequestHeader('X-NewRelic-ID', 'VQECWF5UChAHUlNTBwgBVw==');
-    httpClient.setRequestHeader('x-nba-stats-token', 'true');
     httpClient.send();
 }
 
